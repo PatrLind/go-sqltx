9cedf6161c23d5f8b5be69bb3925de651541b604
Patrik Lindahl <patrik@makersign.com>
2021-01-29 14:11:36 +0800

    Added missing context functions and rearranged the interfaces

--- sqltxx/querier.go
+++ sqltxx/querier.go
@@ -1,6 +1,7 @@
 package sqltxx
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/jmoiron/sqlx"
@@ -29,30 +30,64 @@ type Querier interface {
 	// Any named placeholder parameters are replaced with fields from arg.
 	NamedExec(query string, arg interface{}) (sql.Result, error)
 
+	// NamedExecContext using this DB.
+	// Any named placeholder parameters are replaced with fields from arg.
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+
 	// Select using this DB.
 	// Any placeholder parameters are replaced with supplied args.
 	Select(dest interface{}, query string, args ...interface{}) error
+
+	// SelectContext using this DB.
+	// Any placeholder parameters are replaced with supplied args.
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 
 	// Get using this DB.
 	// Any placeholder parameters are replaced with supplied args.
 	// An error is returned if the result set is empty.
 	Get(dest interface{}, query string, args ...interface{}) error
 
+	// GetContext using this DB.
+	// Any placeholder parameters are replaced with supplied args.
+	// An error is returned if the result set is empty.
+	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+
 	// Queryx queries the database and returns an *sqlx.Rows.
 	// Any placeholder parameters are replaced with supplied args.
 	Queryx(query string, args ...interface{}) (*sqlx.Rows, error)
+
+	// QueryxContext queries the database and returns an *sqlx.Rows.
+	// Any placeholder parameters are replaced with supplied args.
+	QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error)
 
 	// QueryRowx queries the database and returns an *sqlx.Row.
 	// Any placeholder parameters are replaced with supplied args.
 	QueryRowx(query string, args ...interface{}) *sqlx.Row
 
+	// QueryRowxContext queries the database and returns an *sqlx.Row.
+	// Any placeholder parameters are replaced with supplied args.
+	QueryRowxContext(ctx context.Context, query string, args ...interface{}) *sqlx.Row
+
 	// MustExec (panic) runs MustExec using this database.
 	// Any placeholder parameters are replaced with supplied args.
 	MustExec(query string, args ...interface{}) sql.Result
 
+	// MustExecContext (panic) runs MustExec using this database.
+	// Any placeholder parameters are replaced with supplied args.
+	MustExecContext(ctx context.Context, query string, args ...interface{}) sql.Result
+
 	// Preparex returns an sqlx.Stmt instead of a sql.Stmt
 	Preparex(query string) (*sqlx.Stmt, error)
 
+	// PreparexContext returns an sqlx.Stmt instead of a sql.Stmt.
+	//
+	// The provided context is used for the preparation of the statement, not for
+	// the execution of the statement.
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+
 	// PrepareNamed returns an sqlx.NamedStmt
 	PrepareNamed(query string) (*sqlx.NamedStmt, error)
+
+	// PrepareNamedContext returns an sqlx.NamedStmt
+	PrepareNamedContext(ctx context.Context, query string) (*sqlx.NamedStmt, error)
 }
